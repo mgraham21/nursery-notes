fe763fb248506334b0cafbf9da149437bf7d8ee3
Mariah Chavez-Graham <mgraham21@cnm.edu>
2020-02-25 15:53:21 -0700

    Created repositories

---
+++ app/src/main/java/net/nurserynotes/model/repository/ChildRepository.java
@@ -0,0 +1,5 @@
+package net.nurserynotes.model.repository;
+
+public class ChildRepository {
+
+}
--- app/src/main/java/net/nurserynotes/service/NurseryNotesDatabase.java
+++ app/src/main/java/net/nurserynotes/service/NurseryNotesDatabase.java
@@ -1,10 +1,15 @@
 package net.nurserynotes.service;
 
+import android.app.Application;
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 import androidx.room.TypeConverter;
 import androidx.room.TypeConverters;
 import java.util.Date;
+import net.nurserynotes.model.dao.ActivityDao;
+import net.nurserynotes.model.dao.ChildDao;
+import net.nurserynotes.model.dao.RecordDao;
 import net.nurserynotes.model.entity.Activity;
 import net.nurserynotes.model.entity.Child;
 import net.nurserynotes.model.entity.Record;
@@ -18,9 +23,28 @@ import net.nurserynotes.service.NurseryNotesDatabase.Converters;
 @TypeConverters({Converters.class})
 public abstract class NurseryNotesDatabase extends RoomDatabase {
 
+  private static final String DB_NAME = "db";
+
+  private static Application context;
+
+  public static void setContext(Application context) { NurseryNotesDatabase.context = context; }
+
+  public static NurseryNotesDatabase getInstance() { return  InstanceHolder.INSTANCE; }
+
+  public abstract ActivityDao getActivityDao();
+
+  public abstract ChildDao getChildDao();
+
+  public abstract RecordDao getRecordDao();
+
+  private static class InstanceHolder {
+
+    private static final NurseryNotesDatabase INSTANCE = Room.databaseBuilder(
+        context, NurseryNotesDatabase.class, DB_NAME)
+        .build();
+  }
 
   public static class Converters {
-
     @TypeConverter
     public static Long fromDate(Date date) {
       return (date != null) ? date.getTime() : null;
