ef6aa079ca543687515981f420d8f0c900cce73a
Mariah Chavez-Graham <mgraham21@cnm.edu>
2020-02-26 07:19:25 -0700

    Added database/dao to application

--- app/src/main/java/net/nurserynotes/NurseryNotesApplication.java
+++ app/src/main/java/net/nurserynotes/NurseryNotesApplication.java
@@ -2,6 +2,7 @@ package net.nurserynotes;
 
 import android.app.Application;
 import com.facebook.stetho.Stetho;
+import io.reactivex.schedulers.Schedulers;
 import net.nurserynotes.model.repository.ActivityRepository;
 import net.nurserynotes.model.repository.ChildRepository;
 import net.nurserynotes.service.NurseryNotesDatabase;
@@ -15,6 +16,9 @@ public class NurseryNotesApplication extends Application {
     NurseryNotesDatabase.setContext(this);
     ActivityRepository.setContext(this);
     ChildRepository.setContext(this);
+    NurseryNotesDatabase.getInstance().getActivityDao().delete()
+        .subscribeOn(Schedulers.io())
+        .subscribe();
   }
 
 }
