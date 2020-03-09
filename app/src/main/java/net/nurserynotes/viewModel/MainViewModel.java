2939646b867bdba4e3e7ee487f12af2715d0cfc6
Mariah Chavez-Graham <mgraham21@cnm.edu>
2020-03-09 10:13:19 -0600

    Changed color scheme

---
+++ app/src/main/java/net/nurserynotes/viewModel/MainViewModel.java
@@ -0,0 +1,16 @@
+package net.nurserynotes.viewModel;
+
+import android.app.Application;
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+public class MainViewModel extends AndroidViewModel {
+
+  public MainViewModel(@NonNull Application application) {
+    super (application);
+
+  }
+
+  //TODO Add from Notes MainViewModel
+
+}
